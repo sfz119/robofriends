f63d3ff62e4e2b139c295c4eb5f0cb518bf720b6
sfz119 <selferns119@gmail.com>
2020-04-22 16:20:57 +0530

    tested bugs

--- src/container/App.js
+++ src/container/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 	componentDidMount (){ 
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
-		.then(users => this.setState({robots: robots}));
+		.then(users => this.setState({robots: users}));
 	}
 
 	onSearchChange = (event) =>{
